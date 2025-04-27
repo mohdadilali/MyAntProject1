0c04ff0740408b13b14cf192911b96766fce1cd0
Adil Ali <adduali2908@gmail.com>
2025-04-27 16:34:56 +0530

    new update made

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("Hello, Welcome to Jenkins and Ant!");
 		System.out.println("Hello, I am Adil Ali, this is the new update");
 		System.out.println("Hello, I am Adil Ali, this is the new update I am doing it here, okay.");
-        System.out.println("Let's see a conflict");
+        System.out.println("Let's see a conflict, ok");
     }
 }
